412a7cbabe4e1d50cdf82ef31764eee1928d470f
walalang985 <66872185+walalang985@users.noreply.github.com>
2021-02-18 21:59:06 +0800

    see CHANGELOGS.txt for more information.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,render_template,request,render_template_string
+from flask import Flask,redirect,url_for,render_template,request
 from scripts import *
 __NULL__ = ""
 app = Flask(__name__)
@@ -10,18 +10,21 @@ def send_appreciation(test):
     mailer = emailing()
     mailer.send_thank_you(email=email)
     return redirect('/contact_us')
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
-    return redirect('/home', code=302)
+    return redirect(url_for('home',content="home",isloggedin=0))
 @app.route('/home',methods=["GET","POST"])
-@app.route('/home/<int:login>/<string:username>')
-def home(login=0,username=__NULL__):
+def home():
     #0 if the user is not logged in
     #1 if the user is logged in
-    if login == 0 and username == __NULL__:
-        return render_template('main.html'),200
-    else:
-        return render_template('main-login-success.html',username=username),200
+    content = request.args.get('content')
+    isloggedin = request.args.get('isloggedin')
+    username = request.args.get('username')
+    return render_template('main.html',page_content=content,isloggedin=isloggedin,username=username),200
+@app.route('/register',methods=["GET","POST"])
+def register():
+    hotel = request.args
+'''
 @app.route('/register_user_hotel', methods=["GET","POST"])
 def register_user_hotel():
     if request.method == "POST":
@@ -39,6 +42,7 @@ def login_auth():
         email = request.form.get('email')
         passw = request.form.get('password')
         if hotel.login_auth(email, passw):
-            return redirect('/home')
+            return redirect('/main')
+'''
 if __name__ == '__main__':
     app.run(debug=True)
